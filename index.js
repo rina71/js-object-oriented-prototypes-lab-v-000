d99e77a20ae6ffcba4a54992214728d7155c7a12
Yurina Shin <yurinas@gmail.com>
2020-05-17 04:15:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function BoardMember(name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
-
-  this.veto = function(){
+}
+  BoardMember.prototype.veto = function(){
     return 'No, I must disagree'
   }
   this.approve = function(){
